f4d73930cca96c48c8a7c78e0ff1516aceaaeda0
anor23 <burgchiken@gmail.com>
2025-09-20 20:02:19 +0300

    создан базовый круд и базовые модели  сериализаторы для книг

--- books/views.py
+++ books/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
+from rest_framework.generics import ListAPIView, CreateAPIView,UpdateAPIView,DestroyAPIView
+
+from books.serializers import BookSerializer
+from books.models import Book
 
 # Create your views here.
+
+
+class ListBooks(ListAPIView):
+    model = Book
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+
+class BookCreateView(CreateAPIView):
+    model = Book
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+
+class BookUpdateView(UpdateAPIView):
+    model = Book
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+
+class BookDeleteView(DestroyAPIView):
+    model = Book
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
