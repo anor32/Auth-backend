f3a462767007859f0ed292540ab2e7e865b360ea
anor23 <burgchiken@gmail.com>
2025-09-20 12:57:29 +0300

    Cоздана модель пользователя  создан менеджер Пользователей

--- users/models.py
+++ users/models.py
@@ -1,3 +1,52 @@
+from django.contrib.auth.base_user import AbstractBaseUser, BaseUserManager
+from django.contrib.auth.models import PermissionsMixin
 from django.db import models
 
 # Create your models here.
+class UserManager(BaseUserManager,PermissionsMixin):
+    use_in_migrations = True
+
+    def _create_user(self, email, password, **extra_fields):
+        if not email:
+            raise ValueError('Email обязателен')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_superuser', False)
+        extra_fields.setdefault('is_staff', False)
+        return self._create_user(email, password, **extra_fields)
+
+    def create_superuser(self, email, password, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser должен иметь is_staff=True.')
+
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self._create_user(email, password, **extra_fields)
+
+class User(AbstractBaseUser):
+    ROLE_CHOICES = (
+        ('Admin', 'Администратор'),
+        ('MODERATOR', 'Менеджер'),
+        ('USER', 'Пользователь'),
+    )
+
+
+    first_name = models.CharField(verbose_name='Имя',max_length=150)
+    last_name = models.CharField(verbose_name='Фамилия',max_length=150)
+    middle_name = models.CharField(verbose_name='Отчество',max_length=150,blank=True,null=True)
+    email = models.EmailField(verbose_name="почта",unique=True,max_length=250)
+    role = models.CharField(max_length=15, choices=ROLE_CHOICES, default="USER")
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserManager()
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['first_name', 'last_name']
