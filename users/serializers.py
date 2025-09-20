97cbecdfbba27699c9998da406f0a015fe832072
anor23 <burgchiken@gmail.com>
2025-09-20 14:27:24 +0300

    написаны базовые сериализаторы для входа регитсрации пользователя написано Эндпоинты  apiview  входа и регистрации

--- users/serializers.py
+++ users/serializers.py
@@ -1,3 +1,28 @@
 from rest_framework import serializers
+
+from users.models import User
+
+
+class UserRegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, required=True, min_length=8)
+
+    class Meta:
+        model = User
+        fields = ('email', 'first_name', 'last_name','password')
+
+    def create(self, validated_data):
+        password = validated_data.pop('password')
+        user = User(**validated_data)
+        user.set_password(password)
+        user.save()
+        return user
+
+
 class UserLoginSerializer(serializers.ModelSerializer):
-    pass
+    model = User
+    fields = ('email','password')
+
+
+class UserProfileSerializer(serializers.ModelSerializer):
+    model = User
+    fields = ('email', 'first_name', 'last_name', 'middle_name')
