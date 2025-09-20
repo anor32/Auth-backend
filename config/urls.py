eafdea111016a62d53653d6e5187413101416ec5
anor23 <burgchiken@gmail.com>
2025-09-20 19:42:49 +0300

    подлюкчен jwt-token от библиотеки simplejwt

--- config/urls.py
+++ config/urls.py
@@ -16,7 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from rest_framework_simplejwt.views import TokenVerifyView,TokenRefreshView,TokenObtainPairView
 
 urlpatterns = [
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/token/verify/', TokenVerifyView.as_view(), name='token_refresh'),
+
     path('admin/', admin.site.urls),
 ]
