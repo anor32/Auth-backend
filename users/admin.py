f67e63d398c81aab4de0a5594b4cd8f58765428a
anor23 <burgchiken@gmail.com>
2025-09-21 19:26:06 +0300

    в админку добавлены методы для назначения ролей

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth import get_user_model
+from users.models import Role
 
-# Register your models here.
+@admin.register(Role)
+class RoleAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'email', 'role')
+    list_editable = ('role',)
